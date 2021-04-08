9b48516fc8a3a4f71e1d6b519780033640c4a720
Pablo <pablocriadoalbillos@gmail.com>
2021-04-08 12:56:02 +0200

    Add inference

--- segmentation/inference.py
+++ segmentation/inference.py
@@ -6,24 +6,31 @@ from compress_pickle import load
 
 model = tf.keras.models.load_model('./gen/model')
 
-img = cv2.imread('./gen/test_img.png',0)/255
+img = cv2.imread('./gen/test3.jpg', 0)
+img = cv2.resize(img, (192, 192))/255
 
 # (train_x, train_y, val_x, val_y) = load(
 #     './gen/mask_data.pickle', compression='lz4')
 
-input = np.array([img]).reshape((1,128,128,1))
-# input = val_x[:1]
+input = np.array([img]).reshape((1, 192, 192, 1))
+# input = val_x[:10]
 # cv2.imwrite('./gen/test.png', val_x[0]*255)
 print(input.shape)
 
-inference = model(input)[0]
-processed = np.argmax(inference, axis=2)
+inference = model(input)
+processed = np.argmax(inference, axis=3)
+processed = processed.reshape(-1, 96, 96, 1)
+# cv2.imshow('output', processed)
 
-fig=plt.figure(figsize=(8, 8))
-fig.add_subplot(1,2,1)
+fig = plt.figure(figsize=(8, 8))
+fig.add_subplot(3, 2, 1)
 plt.imshow(input[0])
-fig.add_subplot(1,2,2)
-plt.imshow(processed)
+fig.add_subplot(3, 2, 2)
+plt.imshow(processed[0])
+for i in range(3):
+    fig.add_subplot(3, 2, 3+i)
+    plt.imshow(inference[0, :, :, i*3: (i+1)*3])
+# plt.savefig('./gen/foo.png')
 plt.show()
 
 exit()
@@ -38,7 +45,7 @@ for a in range(rows):
 
 for a in range(rows):
     axes.append(fig.add_subplot(rows, 4, a*4+2))
-    plt.imshow(val_y[a, :, :, :3])
+    plt.imshow(val_y[a, :, :, :3]*255)
     plt.axis('off')
 
 for a in range(rows):
@@ -51,4 +58,5 @@ for a in range(rows):
     plt.imshow(processed[a, :, :])
     plt.axis('off')
 fig.tight_layout()
+# plt.savefig('./gen/foo.png')
 plt.show()
