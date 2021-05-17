488511e9e6b0f4635f794a5fc888b23c120ebaf8
MiguelVeganzones <58487867+MiguelVeganzones@users.noreply.github.com>
2021-05-17 22:27:24 +0200

    with bounding boxes and tags

--- segmentation/read_numbers.py
+++ segmentation/read_numbers.py
@@ -7,6 +7,8 @@ X_OFF_WEIGHT = 1
 Y_OFF_WEIGHT = 3
 MAX_SQ_DISTANCE = 25**2
 
+dx ,dy = 3, 3 #extra for rectangles
+
 MIN_SIZE = 10
 
 OPENING = False
@@ -22,73 +24,99 @@ def nearest_nonzero_idx(a, x,y):
     return r[min_idx], c[min_idx]
 
 
-def read_numbers(img_tags):
-    h,w = img_tags.shape
-    _, img_bin = cv2.threshold(img_tags,0,255,cv2.THRESH_BINARY)
-    if OPENING: img_bin = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN, OPENING_KERNEL)
-
-    num_nums, img_labels, stats, centroids = cv2.connectedComponentsWithStats(img_bin, connectivity  = 4)
-    
-    val_and_centroids = np.zeros((num_nums, 3), np.int16)
-
-    sizes = stats[:,-1]
+def read_numbers(img, img_labels_bin):
     j = 0
-    for i in range(1, num_nums):
-        if  sizes[i] > MIN_SIZE: 
-            nx, ny = centroids[i]
-            y, x = nearest_nonzero_idx(img_tags, ny, nx)
+    val_and_centroids = []
 
-            value = img_tags[y, x] - 1 # add -1 to get the real value 
-            val_and_centroids[j] = (value, ny, nx)
-            j+=1
+    for n in range(10):
+        img_bin = img_labels_bin[:,:,n+1]
+        if OPENING: img_bin = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN, OPENING_KERNEL)
 
-    num_stats = np.array(sorted(val_and_centroids[:j], key=lambda x: x[2]))
+        num_nums, img_labels, stats, centroids = cv2.connectedComponentsWithStats(img_bin, connectivity  = 4)
+        #    cv2.CC_STAT_LEFT The leftmost (x) coordinate which is the inclusive start of the bounding box in the horizontal direction.
+        #    cv2.CC_STAT_TOP The topmost (y) coordinate which is the inclusive start of the bounding box in the vertical direction.
+        #    cv2.CC_STAT_WIDTH The horizontal size of the bounding box
+        #    cv2.CC_STAT_HEIGHT The vertical size of the bounding box
+        #    cv2.CC_STAT_AREA The total area (in pixels) of the connected component
+        sizes = stats[:,-1]
 
-    first = 1
-    nums = []
-    pos = []
+        for i in range(1, num_nums):
+            if  sizes[i] > MIN_SIZE: 
+                nx, ny = centroids[i]
+                s = stats[i]
+                #top_left = (stats[i,1], stats[i,0])
+                #bot_right = (stats[i,1]+stats[i,3], stats[i,0]+stats[i,2])
 
-    next_val, next_y, next_x = None, None, None
+                val_and_centroids.append([n, ny, nx, s[1], s[0], s[1]+s[3], s[0]+s[2]])
+                
+                j+=1
 
-    while(len(num_stats)):
-        if first:
-            _val, _y, _x = num_stats[0]
-            num_stats = np.delete(num_stats, 0, axis=0)
-        else:
-            _val, _y, _x = next_val, next_y, next_x
+    if j != 0:
+        num_stats = np.array(sorted(val_and_centroids[:j], key=lambda x: x[2])).astype(np.uint16)
+
+        first = 1
+        nums = []
+        pos = []
+
+        next_val, next_y, next_x, next_bot, next_top = \
+            None, None, None, None, None
+
+        while(len(num_stats)):
+            if first:
+                _val, _y, _x, _top_y, _top_x, _bot_y, _bot_x = num_stats[0]
+                _bot = [_bot_x*2+dx, _bot_y*2+dy]
+                _top = [_top_x*2-dx, _top_y*2-dy]
+                num_stats = np.delete(num_stats, 0, axis=0)
+            else:
+                _val, _y, _x, _top, _bot = next_val, next_y, next_x, next_top, next_bot
         
-        if first:
-            nums.append(str(_val))
-            pos.append([_x, _y])
+            if first:
+                nums.append(str(_val))
+                pos.append([_top, _bot])
 
-        if len(num_stats) != 0:
-            all_x = num_stats[:,2].astype(np.int64) #vector of all remaining numbers x coordinates
-            all_y = num_stats[:,1].astype(np.int64) #vector of all remaining numbers y coordinates
+            if len(num_stats) != 0:
+                all_x = num_stats[:,2].astype(np.int64) #vector of all remaining numbers x coordinates
+                all_y = num_stats[:,1].astype(np.int64) #vector of all remaining numbers y coordinates
  
-            distances_sq = (((all_x - _x)*X_OFF_WEIGHT)**2 + ((all_y - _y)*Y_OFF_WEIGHT)**2).astype(np.float)
+                distances_sq = (((all_x - _x)*X_OFF_WEIGHT)**2 + ((all_y - _y)*Y_OFF_WEIGHT)**2).astype(np.float)
 
-            min_idx = distances_sq.argmin()
+                min_idx = distances_sq.argmin()
 
-            next_val, next_y, next_x = num_stats[min_idx]
+                next_val, next_y, next_x, next_top_y, next_top_x, next_bot_y, next_bot_x = num_stats[min_idx]
+                next_top = [next_top_x*2-dx, next_top_y*2-dy]
+                next_bot = [next_bot_x*2+dx, next_bot_y*2+dy]
+
+                if distances_sq[min_idx] < MAX_SQ_DISTANCE and _x < next_x:
+                    nums[-1] += str(next_val)
+                    num_stats = np.delete(num_stats, min_idx, axis=0)
+                    first = 0
+                    pos[-1][-1] = next_bot
+                else: 
+                    first = 1
+
+        img = cv2.merge([img, img, img])
         
-            if distances_sq[min_idx] < MAX_SQ_DISTANCE and _x < next_x:
-                nums[-1] += str(next_val)
-                num_stats = np.delete(num_stats, min_idx, axis=0)
-                first = 0
-            else: 
-                first = 1
+        for n, (a,b) in zip(nums,pos):
+            img = cv2.rectangle(img, tuple(a), tuple(b), color = (0, 1, 0), thickness = 1)
+            cv2.putText(img, str(n), (a[0], a[1]-5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0.1,0.9,0.1), 1)
 
-    plt.figure()
-    plt.imshow(img_tags)
-    plt.title(f'{nums}')
-    return nums, pos
+        #image = cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 1)
+        #cv2.putText(image, 'Fedex', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+
+        plt.figure()
+        plt.imshow(img)
+        plt.title(f'{nums}')
+
+        return nums, pos
   
 exit()
   
 #Test samples
-img_tags = load('composite_img_tags.pkl')[10:]
-  
-for im in img_tags:
-    read_numbers(im)
-     
+img_tags = load('composite_img_tags.pkl')[:10]
+
+(img_set, img_tags) = load('composite_inline_dataset.pkl')
+
+for im, tags in zip(img_set[:10], img_tags[:10]):
+    read_numbers(im, tags)
+
 plt.show()
